cd70db498fbfcb2983008b35a762117b318c5016
Francesco Zaccaria <francesco.zaccaria2006@gmail.com>
2024-11-25 16:50:41 +0100

    introduced the shop

--- api/index.py
+++ api/index.py
@@ -8,15 +8,15 @@ app = flask.Flask(__name__)
 
 #database connection local connection
 # Copy the database to a writable location
-db_path = './mydatabase.db'  # Path in the deployed source
-temp_db_path = '/tmp/mydatabase.db'
+db_path = '../mydatabase.db'  # Path in the deployed source
+#temp_db_path = '/tmp/mydatabase.db'
 
-if not os.path.exists(temp_db_path):
-    print("Copying database")
-    shutil.copy(db_path, temp_db_path)
+#if not os.path.exists(temp_db_path):
+#    print("Copying database")
+#    shutil.copy(db_path, temp_db_path)
 
 def get_db_connection():
-    conn = sqlite3.connect(temp_db_path)
+    conn = sqlite3.connect(db_path)
     print("Opened database successfully")
     return conn
 
@@ -59,7 +59,7 @@ def login_post():
     users = cursor.fetchall()
     conn.close()
     if len(users) > 0:
-        return redirect('/dashboard')
+        return redirect('/showProducts')
     else:
         return render_template('login.html')
 
@@ -71,37 +71,43 @@ def online():
 def tutorial():
     return render_template('tutorial.html')
 
-@app.post('/add')
-def createUser():
-    data = request.form['username']
-    print(data)
+@app.get('/showProducts')
+def showProducts():
     conn = get_db_connection()
-    conn.execute('INSERT INTO users (name) VALUES (?)', (data,))
-    conn.commit()
-    cursor = conn.execute('SELECT * FROM users')
-    users = cursor.fetchall()
+    # create a table
+    cursor = conn.execute('SELECT * FROM products')
+    products = cursor.fetchall()
     conn.close()
     processed_data = []
-    for i in users:
-        processed_data.append(i[0])
-    return render_template('createUser.html', users=processed_data)
-
-@app.get('/showList')
-def showUsers():
-    conn = get_db_connection()
-    cursor = conn.execute('SELECT * FROM users')
-    users = cursor.fetchall()
-    conn.close()
-    processed_data = []
-    for i in users:
-        processed_data.append(i[0])
-
+    for product in products:
+        print(product[2])
+        processed_data.append({
+            "id": product[0],
+            "name": product[1],
+            "link": "img/"+ product[2] + '.webp' if product[2] else "img/default.jpg",
+            "description": product[3]
+        })
     # Create a simple HTML response
-    response = "<h2>Submitted Data:</h2><ul>"
-    for user in processed_data:
-        response += "<li>{}</li>".format(user)
-    response += "</ul>"
-    return response
+    return render_template('showProducts.html', products=processed_data)
+
+@app.get('/showProduct/<int:id>')
+def showProduct(id):
+    conn = get_db_connection()
+    cursor = conn.execute('SELECT * FROM products WHERE id = ?', (id,))
+    product = cursor.fetchone()
+    conn.close()
+    processed_data = {
+        "id": product[0],
+        "name": product[1],
+        "link": "img/"+ product[2] + '.webp' if product[2] else "img/default.jpg",
+        "description": product[3],
+        "price": product[4],
+    }
+    return render_template('showProduct.html', product=processed_data)
+
+@app.get('/confirm-purchase')
+def confirm_purchase():
+    return render_template('confirm-purchase.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=3000, debug=True)
